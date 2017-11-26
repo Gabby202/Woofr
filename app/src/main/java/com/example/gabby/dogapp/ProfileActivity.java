ef312f5bc71a8b5a0cf3cf7ee4c1d309ee261efb
gabby <gabbu202@gmail.com>
2017-11-26 22:22:55 +0000

    working database reference, adds username and address

--- app/src/main/java/com/example/gabby/dogapp/ProfileActivity.java
+++ app/src/main/java/com/example/gabby/dogapp/ProfileActivity.java
@@ -5,19 +5,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import org.w3c.dom.Text;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private DatabaseReference databaseReference;
     private FirebaseAuth firebaseAuth;
     private TextView userEmailTextView;
     private Button logoutButton;
-
+    private EditText usernameEditText;
+    private  EditText addressEditText;
+    private  Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,5 +51,28 @@ public class ProfileActivity extends AppCompatActivity {
                 }
             }
         });
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        usernameEditText = (EditText) findViewById(R.id.userNameEditText);
+        addressEditText = (EditText) findViewById(R.id.addressEditText);
+        saveButton = (Button) findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(view == saveButton) {
+                    saveUserInformation();
+                }
+            }
+        });
+    }
+
+    private void saveUserInformation() {
+        String username = usernameEditText.getText().toString().trim();
+        String address = addressEditText.getText().toString().trim();
+
+        UserInformation userInformation = new UserInformation(username, address);
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        databaseReference.child(user.getUid()).setValue(userInformation);
+        Toast.makeText(this, "Information Saved!", Toast.LENGTH_LONG).show();
     }
 }
---
+++ app/src/main/java/com/example/gabby/dogapp/UserInformation.java
@@ -0,0 +1,20 @@
+package com.example.gabby.dogapp;
+
+/**
+ * Created by Gabby on 11/26/2017.
+ */
+
+public class UserInformation {
+
+    public String username;
+    public String address;
+
+    public UserInformation() {
+
+    }
+
+    public UserInformation(String username, String address) {
+        this.username = username;
+        this.address = address;
+    }
+}
