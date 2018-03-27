92ab1a25014e0b7c7492b930dd68a948cec36ea7
aDzierzak <agata0151@gmail.com>
2018-03-27 22:12:27 +0100

    foundation of displaying walk details working

--- app/src/main/java/com/example/gabby/dogapp/historyRecyclerView/HistoryViewHolders.java
+++ app/src/main/java/com/example/gabby/dogapp/historyRecyclerView/HistoryViewHolders.java
@@ -1,14 +1,15 @@
 package com.example.gabby.dogapp.historyRecyclerView;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
+import com.example.gabby.dogapp.HistorySingleActivity;
 import com.example.gabby.dogapp.R;
 
-/**
- * Created by Gabby on 3/26/2018.
- */
+
 
 public class HistoryViewHolders extends RecyclerView.ViewHolder implements View.OnClickListener{
 
@@ -23,6 +24,11 @@ public class HistoryViewHolders extends RecyclerView.ViewHolder implements View.
 
     @Override
     public void onClick(View v) {
+        Intent intent = new Intent(v.getContext(), HistorySingleActivity.class);
+        Bundle b = new Bundle();
+        b.putString("walkId", walkId.getText().toString());
+        intent.putExtras(b);
+        v.getContext().startActivity(intent);
 
     }
 }
